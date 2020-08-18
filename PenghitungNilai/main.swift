ad907f1686a4204b24a5eaeea699ac56ff12f6f8
Willy Adinata Saragih <willy.adinata@gmail.com>
2020-08-18 15:25:56 +0700

    Initial Commit

---
+++ PenghitungNilai/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PenghitungNilai
+//
+//  Created by Willy Adinata Saragih on 18/08/20.
+//  Copyright © 2020 WAS Tech. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
