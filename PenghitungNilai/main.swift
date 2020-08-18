836d388256d7b7bbb91c879f92acbc2fedd75eed
Willy Adinata Saragih <willy.adinata@gmail.com>
2020-08-18 15:53:05 +0700

    Penambahan Proses Penghitungan Total,Rata-Rata, dan Conver ke bentuk Grade.

--- PenghitungNilai/main.swift
+++ PenghitungNilai/main.swift
@@ -8,5 +8,61 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Selamat Datang! - Input Nilai Mahasiswa")
+print("---------------------------------------")
+print ("Masukkan total nilai yang akan dinilai", terminator: ": "); let total = readLine() ?? ""
+print("---------------------------------------")
 
+if let totalItem : Int = Int(total){
+    var allValues = [Int]()
+    for i in 1...totalItem {
+        print("Masukkanlah item ke \(i)", terminator: ": "); let newItem = Int(readLine() ?? "") ?? 0
+        allValues.append(newItem)
+    }
+    
+    print("---------------------------------------")
+    var sum: Int = 0
+    
+    for (_, value) in allValues.enumerated() {
+        sum += value
+    }
+    
+    let average = sum / totalItem
+    var grade = ""
+    
+    switch average {
+    case 0..<40:
+        grade = "E";
+        break
+    case 41..<50:
+        grade = "D";
+        break
+    case 51..<60:
+        grade = "C";
+        break
+    case 61..<70:
+        grade = "C+";
+        break
+    case 71..<80:
+        grade = "B";
+        break
+    case 81..<90:
+        grade = "B+";
+        break
+    case 91..<100:
+        grade = "A";
+        break
+    default:
+        grade = "Tidak valid";
+    }
+    
+    if grade != "Tidak valid" {
+        print("Anda mendapatkan Grade nilai \(grade) dari total nilai yang ada. \nAnda mendapatkan total nilai \(sum) dengan rata-rata \(average)")
+    } else {
+        print("Nilai tidak valid!")
+    }
+} else {
+    print("Input tidak valid!")
+}
+
+print("---------------------------------------")
